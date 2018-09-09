1f75f4092664ebe6b37514f493db0e2b941e349b
Christopher M Letherbarrow <cmletherbarrow@ak120d-13.wpi.edu>
2018-09-08 20:42:59 -0400

    jk live plot not working, check encoder angles and DH params - hksaperstein

--- src/liveSketch.m
+++ src/liveSketch.m
@@ -5,26 +5,28 @@ constants;
 packets;
 pp = PacketProcessor(myHIDSimplePacketComs);
 
-figure1 = figure;
-hold on;
-grid on;
 
-
-points = pose([0 0 0]);
-R.handle = plot3(points(1,:),points(2,:),points(3,:));
-
-hold on;
-grid on;
-view(3);
-axis([-100 400 -250 250 -100 400]);
 
 try
+    %calibration;
+    figure1 = figure;
+    hold on;
+    grid on;
+
+    calibration;
+    points = pose([0 0 0]);
+    R.handle = plot3(points(1,:),points(2,:),points(3,:),'MarkerFaceColor',[1 0 0],'MarkerEdgeColor',[0 0 1],...
+    'Marker','o',...
+    'Color',[0 1 0]);
+
+    hold on;
+    grid on;
+    view(3);
+    axis([-100 400 -250 250 -100 400]);
     while(1)
         status_return_packet = pp.command(STATUS_ID, status_packet);
-        q = [status_return_packet(1)
-             status_return_packet(2) 
-             status_return_packet(3)]; 
-        points = pose(q);
+        
+        points = pose([status_return_packet(1) status_return_packet(4) status_return_packet(7)]);
         set(R.handle, 'xdata', points(1,:), 'ydata', points(2,:),'zdata', points(3,:));
         drawnow();
         
