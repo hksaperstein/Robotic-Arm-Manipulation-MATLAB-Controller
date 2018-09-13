78dcad03c7dc643035d9c4529814fbee9eeab30e
Max Alan Spiegelhoff <maspiegelhoff@ak120d-07.wpi.edu>
2018-09-13 10:44:44 -0400

    Lab 3, up to end of 3, need to fix theta3 Position

---
+++ src/functions/ikin.m
@@ -0,0 +1,28 @@
+function [angles] = ikin(position)
+px = position(1);
+py = position(2);
+pz = position(3);
+
+sym (pi);
+
+l1 = 135;
+l2 = 175;
+l3 = 169.28;
+l4 = sqrt((px^2)+(py^2)+((pz-l1)^2));
+
+
+    if (l4 > l2 + l3 || l4 < l2 - l3) || (pz < -34.28) || (px < 0) 
+    msg = 'Outside Normal Range';
+    error(msg)
+    else     
+        
+    alpha = atan2((pz-l1),(sqrt((px^2)+(py^2))));
+    beta = acos(((l2^2)+(l4^2)-(l3^2))/(2*l2*l4));    
+    theta1 = atan2(py,px);
+    theta2 = (alpha + beta);
+    theta3 = (pi/2 - acos(((l2^2)+(l3^2)-(l4^2))/(2*l2*l3)));
+    angles = [theta1, theta2, theta3] * (180/pi) * (4096/360);
+    end
+
+end
+
--- src/main.m
+++ src/main.m
@@ -52,33 +52,37 @@ try
   return_pid_config_packet = pp.read(PID_CONFIG_ID);
   disp(pid_config_packet);
   disp(return_pid_config_packet);
-  % Iterate through a sine wave for joint values
+  
+  % Iterate through a sine wave for joint values/Live Plotting
 %   figure3 = figure;
 %   hold on;
 %   grid on;
-   positionPlot = plot(0, 0, 0, 0, 0, 0);
+%    positionPlot = plot(0, 0, 0, 0, 0, 0);
 
-    figure1 = figure;
-    hold on;
-    grid on;
-    points = pose([0 0 0]);
-    R.handle = plot3(points(1,:),points(2,:),points(3,:),'MarkerFaceColor',[1 0 0],'MarkerEdgeColor',[0 0 1],...
-    'Marker','o',...
-    'Color',[0 1 0]);
-    
-    hold on;
-    grid on;
-    view(3);
-    axis([-150 350 -250 250 -100 400]);
+%     figure1 = figure;
+%     hold on;
+%     grid on;
+%     points = pose([0 0 0]);
+%     R.handle = plot3(points(1,:),points(2,:),points(3,:),'MarkerFaceColor',[1 0 0],'MarkerEdgeColor',[0 0 1],...
+%     'Marker','o',...
+%     'Color',[0 1 0]);
+%     
+%     hold on;
+%     grid on;
+%     view(3);
+%     axis([-150 350 -250 250 -100 400]);
     
   tic;
   return_pid_packet = pp.command(PID_ID, pid_packet);
   y1 = double (return_pid_packet(1));
   y2 = double (return_pid_packet(4));
   y3 = double (return_pid_packet(7));
-  points = pose([return_pid_packet(1) return_pid_packet(4) return_pid_packet(7)]);
-  points = double (points);
-  path = animatedline(points(1,4),points(2,4), points(3,4));
+  
+  %Animated Live Plot
+%   points = pose([return_pid_packet(1) return_pid_packet(4) return_pid_packet(7)]);
+%   points = double (points);
+%   path = animatedline(points(1,4),points(2,4), points(3,4));
+
   x = 0;
   trianglePtsx = [0, 200, 0, -200, 200, 0,0];
   trianglePtsy = [0, 0, 0, 0, 0, 0,0];
@@ -87,47 +91,77 @@ try
   start1y= trianglePtsy(1);
   start1z = trianglePtsz(1);
   totalTime = 30;
-  graphTime = tic;
- 
-  for k = (1:6)
-        timer = tic;
-     
-        constantValuesJoint1 = cubicPoly(start1y, trianglePtsy(k+1), 0, totalTime, 0, 0);
-        constantValuesJoint2 = cubicPoly(start1z, trianglePtsz(k+1), 0, totalTime, 0, 0);
-        constantValuesJoint3 = cubicPoly(start1x, trianglePtsx(k+1), 0, totalTime, 0, 0);
-          for j = 1:20
-            time = toc(timer);
-            joint1Q = cubicTrajectory(constantValuesJoint1, time);
-            joint2Q = cubicTrajectory(constantValuesJoint2, time);
-            joint3Q = cubicTrajectory(constantValuesJoint3, time);
-            pid_packet(1) = joint1Q;
-            pid_packet(4) = joint2Q;
-            pid_packet(7) = joint3Q;
-            return_pid_packet = pp.command(PID_ID, pid_packet);
-            
-            y1 = [y1 (double (return_pid_packet(1)))];
-            y2 = [y2 (double (return_pid_packet(4)))];
-            y3 = [y3 (double (return_pid_packet(7)))];
-            x = [x toc(graphTime)];
-        
-            set(positionPlot(1), 'xdata', x, 'ydata', y1);
-            set(positionPlot(2), 'xdata', x, 'ydata', y2);
-            set(positionPlot(3), 'xdata', x, 'ydata', y3);
+  
+  %Time for graph tics
+%   graphTime = tic;
+ %%
+%   for k = (1:6)
+%         timer = tic;
+%      
+%         constantValuesJoint1 = cubicPoly(start1y, trianglePtsy(k+1), 0, totalTime, 0, 0);
+%         constantValuesJoint2 = cubicPoly(start1z, trianglePtsz(k+1), 0, totalTime, 0, 0);
+%         constantValuesJoint3 = cubicPoly(start1x, trianglePtsx(k+1), 0, totalTime, 0, 0);
+%           for j = 1:20
+%             time = toc(timer);
+%             joint1Q = cubicTrajectory(constantValuesJoint1, time);
+%             joint2Q = cubicTrajectory(constantValuesJoint2, time);
+%             joint3Q = cubicTrajectory(constantValuesJoint3, time);
+%             pid_packet(1) = joint1Q;
+%             pid_packet(4) = joint2Q;
+%             pid_packet(7) = joint3Q;
+%             return_pid_packet = pp.command(PID_ID, pid_packet);
+%             
+%             y1 = [y1 (double (return_pid_packet(1)))];
+%             y2 = [y2 (double (return_pid_packet(4)))];
+%             y3 = [y3 (double (return_pid_packet(7)))];
+%             x = [x toc(graphTime)];
+%         
+%             set(positionPlot(1), 'xdata', x, 'ydata', y1);
+%             set(positionPlot(2), 'xdata', x, 'ydata', y2);
+%             set(positionPlot(3), 'xdata', x, 'ydata', y3);
+% 
+%             points = pose([return_pid_packet(1) return_pid_packet(4) return_pid_packet(7)]);
+%             dlmwrite('jointData.csv', [return_pid_packet(1) return_pid_packet(4) return_pid_packet(7) points(1,4) points(3,4) toc(graphTime)], 'delimiter',',','-append');
+%             set(R.handle, 'xdata', points(1,:), 'ydata', points(2,:),'zdata', points(3,:));
+%             addpoints(path,double (points(1,4)), double (points(2,4)), double (points(3,4)));
+%             drawnow();
+%             pause(.5);
+%             
+%           end
+%         start1x = trianglePtsx(k+1);
+%         start1y= trianglePtsy(k+1);
+%         start1z = trianglePtsz(k+1);
+% 
+%   end initialSample = tic;
+%  csvPlot('jointData.csv');
+%% Timestamp Calc Part 1
 
-            points = pose([return_pid_packet(1) return_pid_packet(4) return_pid_packet(7)]);
-            dlmwrite('jointData.csv', [return_pid_packet(1) return_pid_packet(4) return_pid_packet(7) points(1,4) points(3,4) toc(graphTime)], 'delimiter',',','-append');
-            set(R.handle, 'xdata', points(1,:), 'ydata', points(2,:),'zdata', points(3,:));
-            addpoints(path,double (points(1,4)), double (points(2,4)), double (points(3,4)));
-            drawnow();
-            pause(.5);
-            
-          end
-        start1x = trianglePtsx(k+1);
-        start1y= trianglePtsy(k+1);
-        start1z = trianglePtsz(k+1);
 
-  end
-  csvPlot('jointData.csv');
+% fopen('TimestampData.csv','w');
+%   for i = (1:500)
+%        initialSample = tic;
+%        return_status_packet = pp.command(STATUS_ID, status_packet);
+%        stamp =  toc(initialSample);
+%        dlmwrite('TimestampData.csv', stamp, 'delimiter', ',', '-append');
+%   end
+%   array= csvread('TimestampData.csv');
+%   histogram(array);
+%   arraySTD = std(array)
+%   arrayAvg = mean(array)
+%   arrayMax = max(array)
+%   arrayMin = min(array)
+
+%% Inverse Kinematics Setpoints
+pos1 = ikin([175,0,-34.28]);
+pos2 = ikin([0,0,479.28])
+
+  pid_packet(1) = pos2(1);
+  pid_packet(4) = pos2(2);
+  pp.write(PID_ID, pid_packet);
+  pause(5);
+  pid_packet(7) = pos2(3);
+  pp.write(PID_ID, pid_packet);
+
 catch exception
     getReport(exception)
     disp('Exited on error, clean shutdown');
