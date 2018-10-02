43d9c2da8694105b84cc7f8f7a6893650b7b4ddf
Max Alan Spiegelhoff <maspiegelhoff@ak120d-06.wpi.edu>
2018-10-01 22:18:54 -0400

    Live quiver plot w/ force tips

--- src/main.m
+++ src/main.m
@@ -21,23 +21,41 @@ try
     packets; 
     calibration;
     pidConfiguration;
-     while(1)
-         return_status_packet = statusCom(pp, STATUS_ID, status_packet);
-         jacobian = jacob0([return_status_packet(1) return_status_packet(4) return_status_packet(7)]);
-         torqueJ1 = return_status_packet(3);
-         torqueJ2 = return_status_packet(6);
-         torqueJ3 = return_status_packet(9);
-         torques = [torqueJ1 torqueJ2 torqueJ3];
-         inverseForce(torques, jacobian(1:3,1:3))
-         
-     end
+    tic;
+    figure1 = figure;
+    hold on;
+    grid on;
+    view(3);
+    axis([-0 400 -400 400 -50 450]);
+    xlabel({'X Position (mm)'});
+    zlabel({'Z Position (mm)'});
+    ylabel({'Y Position (mm)'});
+    title({'3D Stick Plot of Arm'});
+    
+    points = pose([0 0 0]);
+    R.handle = plot3(points(1,:),points(2,:),points(3,:),'MarkerFaceColor',[1 0 0],'MarkerEdgeColor',[0 0 1], 'Marker','o', 'Color',[0 1 0]);
+    Q.handle = quiver3(points(1,4), points(2,4), points(3,4),0, 0, 0, 'MaxHeadSize', 200);
+    while(1)
+        time = toc(tic);
+        return_status_packet = statusCom(pp, STATUS_ID, status_packet);
+        jacobian = jacob0([return_status_packet(1) return_status_packet(4) return_status_packet(7)]);
+        torqueJ1 = return_status_packet(3);
+        torqueJ2 = return_status_packet(6);
+        torqueJ3 = return_status_packet(9);
+        torques = [torqueJ1 torqueJ2 torqueJ3] * 100;
+        tipForces = inverseForce(torques, jacobian(1:3,1:3))*100;
+        points = pose([return_status_packet(1) return_status_packet(4) return_status_packet(7)]);
+        set(R.handle, 'xdata', points(1,:), 'ydata', points(2,:),'zdata', points(3,:));
+        set(Q.handle, 'xdata', points(1,4), 'ydata', points(2,4),'zdata', points(3,4),'UData', double(tipForces(1)), 'VData', double(tipForces(2)), 'WData', double(tipForces(3)), 'MaxHeadSize', 200);
+        drawnow()
+    end
     
     
     
 %% Forward Velocity Kinematics
     %forwardVelocityScript
 %% Inverse Velocity Kinematics
-    inverseVelocityScript
+   % inverseVelocityScript
 %% Timestamp Calc Part 1
     %timeStamp
 %% Inverse Kinematics Setpoints
